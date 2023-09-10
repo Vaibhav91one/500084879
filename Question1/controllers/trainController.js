ddf1177e157b86b7595c2394871c51e8ff3de702
Vaibhav91one <vtking03@gmail.com>
2023-09-10 13:18:48 +0530

    Get single train complete

--- Question1/controllers/trainController.js
+++ Question1/controllers/trainController.js
@@ -3,34 +3,56 @@ const request = require("request")
 
 const sendAuthRequest = async () => {
     const body = JSON.stringify({
-      companyName: "Train Drag",
-      clientID: "c814790d-1fe2-408d-86bc-e9c211de7fd1",
-      clientSecret: "rTgqubNiIzWcEZsd",
-      ownerName: "Vaibhav",
-      ownerEmail: "500084879@stu.upes.ac.in",
-      rollNo: "500084879",
+        companyName: "Train Drag",
+        clientID: "c814790d-1fe2-408d-86bc-e9c211de7fd1",
+        clientSecret: "rTgqubNiIzWcEZsd",
+        ownerName: "Vaibhav",
+        ownerEmail: "500084879@stu.upes.ac.in",
+        rollNo: "500084879",
     });
-  
-    const response = await fetch('http://20.244.56.144/train/auth', {
-      method: 'POST',
-      body,
-    });
-  
-    if (response.status === 200) {
-      // The request was successful.
-      const data = await response.json();
-      return data.access_token;
-    } else {
-      // The request failed.
-      return false;
-    }
-  };
 
-  const getTrainsReq = async () => {
+    const response = await fetch('http://20.244.56.144/train/auth', {
+        method: 'POST',
+        body,
+    });
+
+    if (response.status === 200) {
+        // The request was successful.
+        const data = await response.json();
+        return data.access_token;
+    } else {
+        // The request failed.
+        return false;
+    }
+};
+
+const getTrainsReq = async () => {
     const accessToken = await sendAuthRequest();
-    console.log(accessToken)
-  
+    if (!accessToken) {
+        return
+    }
+
     const response = await fetch('http://20.244.56.144/train/trains', {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+
+    if (response.status === 200) {
+        // The request was successful.
+        const data = await response.json();
+        return data;
+    } else {
+        // The request failed.
+        throw new Error(response.statusText);
+    }
+};
+
+const getTrainRequest = async (path) => {
+    const accessToken = await sendAuthRequest();
+  
+    const response = await fetch(`http://20.244.56.144/train/trains/${path}`, {
       method: 'GET',
       headers: {
         Authorization: `Bearer ${accessToken}`,
@@ -47,21 +69,24 @@ const sendAuthRequest = async () => {
     }
   };
 
-//@desc Get all contacts
-//@route GET /api/contacts
+
+
+//@desc Get all trains
+//@route GET /api/train
 //@access private
 const getTrains = asyncHandler(async (req, res) => {
     let trains = await getTrainsReq();
-    console.log(trains)
     // const contacts = await Contact.find({user_id: req.user.id});
     res.status(200).json(trains);
 });
 
-//@desc Get Contact
-//@route GET /api/contacts
+//@desc Get train
+//@route GET /api/train/{train_id}
 //@access private
 const getTrain = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Working getTrain" });
+    const path = req.params.id;
+    let train = await getTrainRequest(path);
+    res.status(200).json(train);
 });
 
 module.exports = { getTrains, getTrain }; 
--- Question1/routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {loginUser} = require('../controllers/userController');
-// const { validateToken } = require('../middleware/ValidateTokenHandle');
-const router = express.Router();
-
-router.post("/login", loginUser);
-
-module.exports = router;
